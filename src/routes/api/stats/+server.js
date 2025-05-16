5ad12f1c7b147bebf3bbbf4db2a327986cd85056
OfficialAudite <dovydas.bumblauskas@aol.com>
2025-05-16 11:48:11 +0200

    Update to mitigate SSRF attacks

--- src/routes/api/stats/+server.js
+++ src/routes/api/stats/+server.js
@@ -1,9 +1,19 @@
 export async function GET({ url }) {
-    const targetUrl = url.searchParams.get('targetUrl') ?? "";
+    const targetUrl = url.searchParams.get('targetUrl');
 
     // Check if the targetUrl is within the allowed domain (newbieland.net and its subdomains)
     const allowedDomain = 'newbieland.net';
     const isDomainAllowed = targetUrl.startsWith(`https://${allowedDomain}/`) || targetUrl.startsWith(`https://*.${allowedDomain}/`);
+
+    // Additional check to ensure the targetUrl is a valid URL
+    try {
+        new URL(targetUrl);
+    } catch (error) {
+        return new Response(JSON.stringify({ error: 'Invalid target URL' }), {
+            status: 400,
+            headers: { 'Content-Type': 'application/json' },
+        });
+    }
 
     if (!isDomainAllowed) {
         return new Response(JSON.stringify({ error: 'Invalid target URL' }), {
