8d7fef343d31ad9796a9bef46c01be39e4f4030b
OfficialAudite <dovydas.bumblauskas@aol.com>
2025-08-24 12:34:03 +0200

    Update team member details with placeholder information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,9 +86,9 @@
 
         <div class="p-2 lg:w-1/3 md:w-1/2 w-full lg:mx-auto">
           <div class="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://screeps.com/api/user/badge-svg?username=ScarlettCatte">
+            <img alt="team" class="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://placehold.co/128x128">
             <div class="flex-grow">
-              <h2 class="text-white title-font font-medium">Scarlett</h2>
+              <h2 class="text-white title-font font-medium">Placeholder</h2>
               <p class="text-gray-400">Newbie Land Discord Moderator</p>
             </div>
           </div>
